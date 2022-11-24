3981a09e0048b2ede916f4312f95cd3b652278bb
Konstantin Shidlauskas <koktan93@mail.ru>
2022-11-24 18:46:17 +0200

    change
    ja dopisal sistemu boja!!!

--- src/game/KostjaTestClass.java
+++ src/game/KostjaTestClass.java
@@ -66,71 +66,157 @@ public class KostjaTestClass {
 
         // нажми цифру 1 в консоле для удара или цифру 2 для удара .
 
-            int playerHealt = 100;
-            int opHealth = 100;
-            boolean game = true;
-            while (game) {
-                try {
-                    Scanner scanner = new Scanner(System.in);
-                    int num1 = scanner.nextInt();
-                    if (num1 == 1) {
-                        System.out.println("You hit your opponent");
-                        System.out.println(" ");
-
-                        Random random1 = new Random();
-                        int punchAttack;
-                        punchAttack = random1.nextInt(5);
-                        switch (punchAttack) {
-                            case 0:
-                                for (int i = 1; i <= 15; i++) {
-                                    opHealth--;
-                                }
-                                System.out.println("You hitted your opponent");
-                                break;
-                            case 1:
-                                for (int i = 1; i <= 15; i++) {
-                                    opHealth--;
-                                }
-                                System.out.println("You hitted your opponent into FACE!");
-                                break;
-                            case 2:
-                                for (int i = 1; i <= 15; i++) {
-                                    opHealth--;
-                                }
-                                System.out.println("You hitted your opponent between TWO LEGS");
-                                break;
-                            case 3:
-                                for (int i = 1; i <= 15; i++) {
-                                    opHealth--;
-                                }
-                                System.out.println("You hitted your opponent OOOUUcccchhh");
-                                break;
-                            case 4:
-                                for (int i = 1; i <= 30; i++) {
-                                    opHealth--;
-                                }
-                                System.out.println("Critical HIT!!!!");
-                                break;
-
-                            default:
-                                break;
-                        }
-
-                    } else if (num1 == 2) {
-                        System.out.println("you kicked your opponent ");
-                        System.out.println(" ");
-                        Random random2 = new Random();
-                        int kickAttack;
-                        kickAttack = random2.nextInt();
-
-                    }
-                    System.out.println("Your opponets healts is : " + opHealth);
+        int playerHealt = 100;
+        int opHealth = 100;
+        boolean game = true;
+        while (game) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                int num1 = scanner.nextInt();
+                if (num1 == 1) {
+                    System.out.println("You hit your opponent");
                     System.out.println(" ");
-                } catch (InputMismatchException e) {
+
+                    Random random1 = new Random();
+                    int punchAttack;
+                    punchAttack = random1.nextInt(5);
+                    switch (punchAttack) {
+                        case 0:
+                            for (int i = 1; i <= 15; i++) {
+                                opHealth--;
+                            }
+                            System.out.println("You hitted your opponent");
+                            break;
+                        case 1:
+                            for (int i = 1; i <= 15; i++) {
+                                opHealth--;
+                            }
+                            System.out.println("You hitted your opponent into FACE!");
+                            break;
+                        case 2:
+                            for (int i = 1; i <= 15; i++) {
+                                opHealth--;
+                            }
+                            System.out.println("You hitted your opponent between TWO LEGS");
+                            break;
+                        case 3:
+                            for (int i = 1; i <= 15; i++) {
+                                opHealth--;
+                            }
+                            System.out.println("You hitted your opponent OOOUUcccchhh");
+                            break;
+                        case 4:
+                            for (int i = 1; i <= 30; i++) {
+                                opHealth--;
+                            }
+                            System.out.println("Critical HIT!!!!");
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                } else if (num1 == 2) {
+                    System.out.println("you kicked your opponent ");
+                    System.out.println(" ");
+                    Random random2 = new Random();
+                    int kickAttack;
+                    kickAttack = random2.nextInt();
+
                 }
+                System.out.println("Your opponets healts is : " + opHealth);
+                System.out.println(" ");
+
+                if (opHealth <= 0) {
+                    System.out.println("YOU KILL THE ENEMIE!!");
+                    game = false;
+                    System.exit(0);
+                }
+
+
+            } catch (InputMismatchException e) {
             }
 
+            // ataka vraga
+
+            Random random2 = new Random();
+            int attack;
+            attack = random2.nextInt(6);
+            switch (attack) {
+                case 0:
+                    System.out.println("Swagger punched you!");
+                    System.out.println("Swagger hits u -10 dmg");
+                    System.out.println(" ");
+                    for (int i = 1; i <= 10; i++) {
+                        playerHealt--;
+                    }
+                    break;
+                case 1:
+                    System.out.println("Swagger punched you!");
+                    System.out.println("Swagger hits u -25 dmg");
+                    System.out.println("CRITICAL DAMAGE");
+                    System.out.println(" ");
+                    for (int i = 1; i <= 25; i++) {
+                        playerHealt--;
+                    }
+                    break;
+                case 2:
+                    System.out.println("Swagger punched you!");
+                    System.out.println("Swagger hits u -13 dmg");
+                    System.out.println(" ");
+                    for (int i = 1; i <= 13; i++) {
+                        playerHealt--;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Swagger punched you!");
+                    System.out.println("Swagger hits u -11 dmg");
+                    System.out.println(" ");
+                    for (int i = 1; i <= 11; i++) {
+                        playerHealt--;
+                    }
+                    break;
+                case 4:
+                    System.out.println("Swagger punched you!");
+                    System.out.println("Swagger hits u -17 dmg");
+                    System.out.println(" ");
+                    for (int i = 1; i <= 17; i++) {
+                        playerHealt--;
+                    }
+                    break;
+                case 5:
+                    System.out.println("Swagger punched you!");
+                    System.out.println("Swagger hits u -14 dmg");
+                    System.out.println(" ");
+                    for (int i = 1; i <= 14; i++) {
+                        playerHealt--;
+                    }
+                    break;
+                case 6:
+                    System.out.println("Swagger punched you!");
+                    System.out.println("Swagger hits u -8 dmg");
+                    System.out.println(" ");
+                    for (int i = 1; i <= 8; i++) {
+                        playerHealt--;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("Your healt is " + playerHealt);
+            if (playerHealt <= 0) {
+                System.out.println("You died! Try again next time");
+                System.out.println(" ");
+
+            } else if (playerHealt > 1) {
+                game = true;
+            }
         }
+
+
     }
+}
+
+
 
 
