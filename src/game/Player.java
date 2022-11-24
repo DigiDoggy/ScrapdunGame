c600b4dd5d780a7cfad079555bcfef4298780513
Ilja Aituganov <Ilja.Aituganov@gmail.com>
2022-11-24 12:30:24 +0200

    Method action1 - Player Class

--- src/game/Player.java
+++ src/game/Player.java
@@ -24,10 +24,38 @@ public class Player extends AbstractPlayers {
         System.out.println("Hello new hero of that danger word.");
         System.out.println("Enter your name: ");
         playerName = scanner.nextLine();
-        System.out.println(playerName + "All around you you see the battlefield and many corpses.\n" +
-                "You understand, you can pick up a Equipment.");
+        System.out.println(playerName + "you see many corpses after the battle, "+"\n" +
+                "an empty military tent and a bush of berries growing next to you.");
+        System.out.println("What will you do?");
 
         return playerName;
+    }
+    //method for user who find some item
+    public static void action1(){
+
+
+        char choice  = (char) scanner.nextInt();
+
+
+        switch (choice){
+            case 1:
+                //some method
+                System.out.println("Examine the corpses");
+                break;
+            case 2:
+                //some method
+                System.out.println("Go check out the tent.");
+                break;
+            case 3:
+                //some method
+                System.out.println("Eat a berry");
+                break;
+            default:
+                // leave method
+                System.out.println("It's time to finish the game!");
+        }
+
+
     }
 
 
