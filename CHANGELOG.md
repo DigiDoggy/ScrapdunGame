55557d9e013d7ca38065faf610abd3fd54ea3bd6
Ilja Aituganov <Ilja.Aituganov@gmail.com>
2022-12-02 23:58:02 +0200

    case 1 case 2 - from Player.action1() works

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ## 0.1.0
 - Configure Maven project
-- 2022.12.02 The first choice of the action1() method works. Calculating damage and defence for player is working. Hava some problem in (if statement Player.action1()). Created file JavaLearner.
+- 2022.12.02 The first choice of the action1() method works. Calculating damage and defence for player is working. Created file JavaLearner.
--- src/main/java/digidoggy/scrapdun/model/Player.java
+++ src/main/java/digidoggy/scrapdun/model/Player.java
@@ -17,26 +17,25 @@ import java.util.Scanner;
  5.
 
  */
-public class Player{
+public class Player {
 
-    public static List<Integer> tag = new ArrayList<>();
 
-    protected static String characterName ;
+    protected static String characterName;
     protected static int health = 100;
-    protected static String characterWeapon ;
-    protected static String armorsHead ;
-    protected static String armorBody ;
-    protected static String armorLegs ;
-    protected static String armorArms ;
+    protected static String characterWeapon;
+    protected static String armorsHead;
+    protected static String armorBody;
+    protected static String armorLegs;
+    protected static String armorArms;
     public static int damage = 10;
-//    protected static int crtDamage = 5;
+    //    protected static int crtDamage = 5;
 //    protected static String block;
     public static int defence = 10;
 
 
-    public Player(String characterName, int health, int damage, int defence){
-        Player.characterName =characterName;
-        Player.health =health;
+    public Player(String characterName, int health, int damage, int defence) {
+        Player.characterName = characterName;
+        Player.health = health;
         Player.damage = damage;
         Player.defence = defence;
 
@@ -45,12 +44,11 @@ public class Player{
                 "damage: " + damage + "\n" +
                 "defence: " + defence);
     }
+
     Player player = new Player(characterName, health, damage, defence);
     public static Scanner scanner = new Scanner(System.in);
 
     public static List<Armor> playerArmor = new ArrayList<>();
-
-
 
 
 //    characterName
@@ -62,63 +60,42 @@ public class Player{
         System.out.println("Hello new hero of that danger word.");
         System.out.println("Enter your name: ");
         characterName = scanner.nextLine();
-        System.out.println(characterName + "you see many corpses after the battle, "+"\n" +
+        System.out.println(characterName + "you see many corpses after the battle, " + "\n" +
                 "an empty military tent and a bush of berries growing next to you.");
         System.out.println("What will you do?");
 
         return characterName;
     }
+
     //method for user who find some item
-    public static void action1(){
+    public static void action1() {
+
+        System.out.println("1.Examine the corpses.");
+        System.out.println("2.Go check out the tent.");
+        System.out.println("3.Go fight");
+        System.out.println("4.It's time to finish the game!");
 
 
-        tag.add(0);
-
-        for (int i= 1; i <=4;i++){
-
-            if(tag.get(0)==0){
-                System.out.println(i + ".Examine the corpses." );
-                System.out.println((i+1) + ".Go check out the tent.");
-                System.out.println((i+2) + ".It's time to finish the game!");
-                break;
-            }else if (tag.get(0)==0 && tag.get(1)==1){
-                System.out.println(i + ".Go check out the tent.");
-                System.out.println((i+1) + ".It's time to finish the game!");
-                break;
-            } else if (tag.get(0)==0 && tag.get(1)==1 && tag.get(2)==2) {
-                System.out.println((i+1) + ".It's time to finish the game!");
-            } else if (tag.get(1)==2) {
-                System.out.println(i + ".Examine the corpses." );
-                System.out.println((i+1) + ".It's time to finish the game!");
-            } else {
-                System.out.println(i + ".It's time to finish the game!");
-            }
-
-        }
+        int choice = Main.validatesInputNumber();
+        Main.choiceFromTo(choice, 1, 4);
 
 
-        int choice  = Main.validatesInputNumber();
-
-        Main.choiceFromTo(choice,1,4);
-
-
-
-        switch (choice){
+        switch (choice) {
             case 1:
-                tag.add(1);
                 Armor.chooseArmor();
-                action1();
                 break;
             case 2:
-                tag.add(2);
-                System.out.println("You enter the tent and immediately pay attention to the chest.");
+                System.out.println("You went inside the tent and saw a note.");
+                System.out.println("And remember the password to let go. \n" +
+                        "JavaEE18");
+                System.out.println();
                 action1();
                 break;
             case 3:
-            tag.add(3);
-            break;
+                System.out.println("Go to fight method");
+                break;
 
-            default:
+            case 4:
                 // leave method
 
                 break;
@@ -126,14 +103,18 @@ public class Player{
     }
 
     //calculate defence
-    public static void calculateDefenceForPlayer(int defOfArmor){
-        Player.defence+=defOfArmor;
+    public static void calculateDefenceForPlayer(int defOfArmor) {
+        Player.defence += defOfArmor;
     }
 
     //calculate damage
 
-    public static void calculateWeaponDamage (int weaponDamage){
-        Player.damage +=weaponDamage;
+    public static void calculateWeaponDamage(int weaponDamage) {
+        Player.damage += weaponDamage;
+    }
+
+    public void tent() {
+        System.out.println("You go to t");
     }
 //
 //    //Random Method
