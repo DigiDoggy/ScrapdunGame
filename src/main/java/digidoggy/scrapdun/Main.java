24cf2db31f719bc4552886773834d9fc2d64cb3e
Ilja Aituganov <Ilja.Aituganov@gmail.com>
2022-12-09 01:54:17 +0200

    test

--- src/main/java/digidoggy/scrapdun/Main.java
+++ src/main/java/digidoggy/scrapdun/Main.java
@@ -17,8 +17,8 @@ public class Main {
 
     public static void main(String[] args)throws SQLException {
 
-//        greetings();
-//        Player.createName();
+        greetings();
+        Player.createName();
         action1();
 
 
