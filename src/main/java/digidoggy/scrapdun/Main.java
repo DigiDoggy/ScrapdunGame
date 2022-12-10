b1d49b2c7de462ad18b224f66fb53f3e762712b9
Ilja Aituganov <Ilja.Aituganov@gmail.com>
2022-12-10 14:32:37 +0200

    Game Complete v1

--- src/main/java/digidoggy/scrapdun/Main.java
+++ src/main/java/digidoggy/scrapdun/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
     public static void main(String[] args)throws SQLException {
 
+        // todo ALL COMPLETED
         greetings();
         Player.createName();
         action1();
